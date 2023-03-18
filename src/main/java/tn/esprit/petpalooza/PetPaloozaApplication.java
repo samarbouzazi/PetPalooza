42a55cecab06a8c30af807e5af8dc2c1f51528ef
samarbouzezi <samar.bouzezi@esprit.tn>
2023-03-18 12:51:18 +0100

    .

---
+++ src/main/java/tn/esprit/petpalooza/PetPaloozaApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.petpalooza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPaloozaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetPaloozaApplication.class, args);
+    }
+
+}
