49848e4599d1ddc52e92fe3237142936a386454d
samarbouzezi <samar.bouzezi@esprit.tn>
2023-03-18 01:15:09 +0100

    First Commit

---
+++ src/main/java/pet/palooza/PetPaloozaApplication.java
@@ -0,0 +1,13 @@
+package pet.palooza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPaloozaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetPaloozaApplication.class, args);
+    }
+
+}
