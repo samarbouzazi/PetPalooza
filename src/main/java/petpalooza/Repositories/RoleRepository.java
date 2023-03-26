5862c6b5525a05761c22cfd4bf2fc12a68ba6f5b
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-03-26 23:28:44 +0300

    adding the user and role Repository

---
+++ src/main/java/petpalooza/Repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package petpalooza.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import petpalooza.Entities.Role;
+import petpalooza.Entities.User;
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
