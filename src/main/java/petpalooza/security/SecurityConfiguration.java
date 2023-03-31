159d1cfdee80be67e2a8b5ee1a1bdfe0b8d76157
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-03-31 15:34:03 +0300

    security config

--- src/main/java/petpalooza/security/SecurityConfiguration.java
+++ src/main/java/petpalooza/security/SecurityConfiguration.java
@@ -1,70 +1,118 @@
 package petpalooza.security;
-import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.security.authentication.AuthenticationManager;
-        import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-        import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-        import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.http.SessionCreationPolicy;
-        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-        import org.springframework.security.crypto.password.PasswordEncoder;
-        import org.springframework.security.web.SecurityFilterChain;
-        import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import petpalooza.security.predifineInterfaces.UserDetailsServiceImpl;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import petpalooza.Repositories.UserRepository;
+import petpalooza.security.jwt.JwtAuthenticationFilter;
+import petpalooza.security.jwt.JwtAuthorizationFilter;
+import petpalooza.security.predifineInterfaces.UserPrincipalDetailsService;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-        // securedEnabled = true,
-        // jsr250Enabled = true,
-        prePostEnabled = true)
-public class SecurityConfiguration {
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
+@EnableWebSecurity
 
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration  {
+
+    private UserRepository userRepository;
+    // private BasicAuthenticationEntryPoint basicAuthenticationEntryPoint;
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService , UserRepository userRepository /*,BasicAuthenticationEntryPoint basicAuthenticationEntryPoint*/) {
+        this.userPrincipalDetailsService = userPrincipalDetailsService;
+        this.userRepository= userRepository;
+        //   this.basicAuthenticationEntryPoint= basicAuthenticationEntryPoint;
+    }
+
+
 
     @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return     http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //remove csrf and state in session because jwt  do not need  them
+
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), this.userRepository))
+                .authorizeRequests()
+                .antMatchers("/login").permitAll().
+                antMatchers("/api/test/user").authenticated().
+//                .antMatchers("/api/test/manager").hasRole("MANAGEMENT").
+                antMatchers("/api/test/admin").hasRole("ADMIN").
+                antMatchers("/api/test/mod").hasRole("MANAGER").
+                and()
+                .build()
+
+                ;
+
+
+                /*
+                .authorizeRequests()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/api/public/test").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
+                .antMatchers("/api/public/users").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/signin")
+                .loginPage("/login").permitAll()
+                .usernameParameter("txtUsername")
+                .passwordParameter("txtPassword")
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+                .and()
+                .rememberMe().tokenValiditySeconds(2592000).key("mySecret!").userDetailsService(userPrincipalDetailsService).rememberMeParameter("checkRememberMe");
+    */
+    }
+
+    //  "/path/**"
+    //the order of antMatchers is so important for example if i put  anyRequest().permetAll() at the beginning of the chain
+
+
+
+
+
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+
+        return daoAuthenticationProvider;
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-
-        return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
-                .anyRequest().authenticated();
 
-        http.authenticationProvider(authenticationProvider());
 
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
+    //Enable SSL/HTTPS
+    /*
+    steps:
+     1)certificate (self signed or buy
+     2) Modify app.properties
+     3)add @bean for ServletWebServerFactory(reddirect all http trafic to https)
+     .\keytool -genkey -alias bootsecurity -storetype PKCS12 -keyalg RSA -Keysize 2048 -keystore bootsecurity.p12 -validity 3650
+     */
 }
