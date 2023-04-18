97e3a0f809fa41207f115edf0bc2b3eb10abd0a5
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-04-19 00:49:17 +0200

    before migration

--- src/main/java/petpalooza/PetPaloozaApplication.java
+++ src/main/java/petpalooza/PetPaloozaApplication.java
@@ -3,10 +3,13 @@ package petpalooza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+//@EnableAspectJAutoProxy
 public class PetPaloozaApplication {
 
     public static void main(String[] args) {
