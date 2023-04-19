b816c771cfa2300254b56a67f577b8ae19c76eaf
zouarislem <islem.zouari@esprit.tn>
2023-04-20 00:32:34 +0100

    NbrOflikes+dislikesDone

--- src/main/java/petpalooza/Repositories/RatingAnimalRepository.java
+++ src/main/java/petpalooza/Repositories/RatingAnimalRepository.java
@@ -15,5 +15,9 @@ public interface RatingAnimalRepository extends JpaRepository<RatingAnimal, Long
     public RatingAnimal likeUserToAnimal (long idAnimal, long idUser);
     @Query("Select r from RatingAnimal r where r.liked = false and r.animal.idAnimal=?1")
     public List<RatingAnimal> dislikesAnimal(long idAnimal);
+    @Query("select COUNT(*) from RatingAnimal r  WHERE r.liked=true and r.animal.idAnimal=?1")
+    public int nbrLikes(long idAnimal);
+    @Query("select COUNT(*) from RatingAnimal r  WHERE r.liked=false and r.animal.idAnimal=?1")
+    public int nbrDisLikes(long idAnimal);
 
 }
