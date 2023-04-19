7bbe19c95735b67c826199e3c77bc3407c5321ae
samarbouzezi <samar.bouzezi@esprit.tn>
2023-04-19 03:24:31 +0100

    search stream 1 critere

--- src/main/java/petpalooza/Services/EmailService.java
+++ src/main/java/petpalooza/Services/EmailService.java
@@ -14,10 +14,9 @@ import java.io.File;
 
 
 @Service
-public class EmailService implements IEmailService {
+public class EmailService implements IEmailService{
 
     private final JavaMailSender javaMailSender;
-
     @Value("${spring.mail.username}")
     private String sender;
 
@@ -27,39 +26,22 @@ public class EmailService implements IEmailService {
 
     @Override
     public String sendSimpleMail(Email details) {
-
-
-        //try {
-
-
             SimpleMailMessage mailMessage
                     = new SimpleMailMessage();
-
-
             mailMessage.setFrom(sender);
             mailMessage.setTo(details.getRecipient());
             mailMessage.setText(details.getMsgBody());
             mailMessage.setSubject(details.getSubject());
-
-
             javaMailSender.send(mailMessage);
             return "Mail Sent Successfully...";
-       // } catch (Exception e) {
-//            return "Error while Sending Mail";
-//        }
+
     }
-
-
     @Override
     public String sendMailWithAttachment(Email details) {
-
         MimeMessage mimeMessage
                 = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper;
-
         try {
-
-
             mimeMessageHelper
                     = new MimeMessageHelper(mimeMessage, true);
             mimeMessageHelper.setFrom(sender);
@@ -67,21 +49,14 @@ public class EmailService implements IEmailService {
             mimeMessageHelper.setText(details.getMsgBody());
             mimeMessageHelper.setSubject(
                     details.getSubject());
-
-
             FileSystemResource file
                     = new FileSystemResource(
                     new File(details.getAttachment()));
-
             mimeMessageHelper.addAttachment(
                     file.getFilename(), file);
-
-
             javaMailSender.send(mimeMessage);
             return "Mail sent Successfully";
         } catch (MessagingException e) {
-
-
             return "Error while sending mail!!!";
         }
     }
