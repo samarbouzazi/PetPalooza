a72a16080fe428c8151fe39035a6b896cf59f78e
samarbouzezi <69418214+samarbouzezi@users.noreply.github.com>
2023-04-19 01:52:45 +0100

    Update UserService.java

--- src/main/java/petpalooza/Services/UserService.java
+++ src/main/java/petpalooza/Services/UserService.java
@@ -1,14 +1,16 @@
-package petpalooza.Services;
+package petpalooza.Services.userServices;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import petpalooza.Entities.Event;
 import petpalooza.Entities.User;
 import petpalooza.Repositories.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Data
@@ -30,7 +32,7 @@ public class UserService implements IUser{
     @Override
     public User addNewUser(User user) {
         String name=user.getUsername();
-        if((userRepository.existsByUsername(name))){
+        if((userRepository.existsByUsername(name)) ==true){
             System.out.println("user exist ");
         }
 
@@ -48,7 +50,6 @@ public class UserService implements IUser{
     public User updateUser(User user) {
         return userRepository.save(user);
     }
-
     @Override
     public void deleteUser(Long idUser) {
         userRepository.deleteById(idUser);
@@ -78,6 +79,51 @@ public class UserService implements IUser{
         }
         return false;
     }
+
+    @Override
+    public void blockUser(Long idUser) {
+       Block(idUser);
+
+
+    }
+
+
+
+    public void Block(Long idUser){
+        User user=new User();
+        user=userRepository.findById(idUser).orElse(null);
+
+        user.setActive(0);
+
+        userRepository.save(user);
+        System.out.println("\n user blocked succesfully!!!");
+
+
+    }
+
+
+    @Override
+    public void SignalerUser(Long idUser) {
+
+        User user=new User();
+        user= findUserByID(idUser);
+        int i= user.getNumberOfSignal();
+        System.out.println("the number of Signal " + i);
+        if(i ==5){
+            Block(idUser);
+            System.out.println("User blocked succusfuly\n -----");
+        }else {
+            int newValue=i+1;
+            user.setNumberOfSignal(newValue);
+            System.out.println("\n  and now is " + i);
+            userRepository.save(user);
+
+            System.out.println(" and know the number of Signal  after saving  " + i);
+
+        }
+    }
+
+
 }
 
 
