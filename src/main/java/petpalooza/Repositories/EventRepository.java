e5260c70867ec84af023a395f7ad8fc5bc63cfca
samarbouzezi <samar.bouzezi@esprit.tn>
2023-04-19 01:37:37 +0100

    integration part2

--- src/main/java/petpalooza/Repositories/EventRepository.java
+++ src/main/java/petpalooza/Repositories/EventRepository.java
@@ -1,6 +1,7 @@
 package petpalooza.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import petpalooza.Entities.Event;
 import petpalooza.Entities.User;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
     List<Event> findAllByOrderByParticipantsDesc();
+    @Query("select e from Event e left join e.participants p group by e order by count(p) desc")
+    List<Event> eventsorted();
 }
