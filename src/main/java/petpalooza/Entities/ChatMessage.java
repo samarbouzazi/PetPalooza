93a675a2ead8d168dada3708d9b4f326e8822b67
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-05-06 14:25:32 +0200

    adding chat

--- src/main/java/petpalooza/Entities/ChatMessage.java
+++ src/main/java/petpalooza/Entities/ChatMessage.java
@@ -5,26 +5,60 @@ import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
 
-
-@Entity
-@Table(name = "chat")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatMessage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long idMessage;
-         String content;
-         Long  userID;
-         MessageType type;
+    private String content;
+    private String sender;
+    private MessageType type;
 
-        public enum MessageType {
-            CHAT, LEAVE, JOIN
-        }
-
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
     }
 
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+}
+
+//@Entity
+//@Table(name = "chat")
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//public class ChatMessage {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Long idMessage;
+//         String content;
+//         Long  userID;
+//         MessageType type;
+//
+//        public enum MessageType {
+//            CHAT, LEAVE, JOIN
+//        }
+//
+//    }
+//
 
--- src/main/java/petpalooza/RestControllers/userController/ChatController/ChatController.java
+++ src/main/java/petpalooza/RestControllers/userController/ChatController/ChatController.java
@@ -5,20 +5,15 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import petpalooza.Entities.ChatMessage;
-import petpalooza.Entities.User;
 
 @Controller
-@RequestMapping("/public/chat")
 public class ChatController {
 
     @MessageMapping("/chat.register")
-    @SendTo("/public/public")
+    @SendTo("/topic/public")
     public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-        User user=new User();
-        headerAccessor.getSessionAttributes().put(/*"username"*/ user.getUsername(), chatMessage.getUserID());
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 
@@ -28,6 +23,5 @@ public class ChatController {
         return chatMessage;
     }
 
-}
 
-
+    }
