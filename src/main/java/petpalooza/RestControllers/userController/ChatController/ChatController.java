c32e4f1082b4e7a698c1aa82d3d1f9b9c7f5a7cb
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-05-06 11:50:10 +0200

    final

--- src/main/java/petpalooza/RestControllers/userController/ChatController/ChatController.java
+++ src/main/java/petpalooza/RestControllers/userController/ChatController/ChatController.java
@@ -5,10 +5,13 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import petpalooza.Entities.ChatMessage;
 import petpalooza.Entities.User;
 
 @Controller
+@RequestMapping("/public/chat")
 public class ChatController {
 
     @MessageMapping("/chat.register")
